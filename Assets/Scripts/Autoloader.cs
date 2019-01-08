15546cdd25cde099d83f36275fb0ad32c264b2f3
jherd23 <jherd23@gmail.com>
2019-01-08 02:44:55 -0500

    Added autoloader

---
+++ Assets/Scripts/Autoloader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Autoloader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel() {
+        int level = 1;
+        if(PlayerPrefs.HasKey("level")) {
+            level = PlayerPrefs.GetInt("level");
+        } else {
+            PlayerPrefs.SetInt("level", 1);
+            PlayerPrefs.Save();
+        }
+        SceneManager.LoadScene("Level" + level);
+    }
+}
