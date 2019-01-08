087340c6bf5645c9e5ac970d87800f0cbdae2bd0
Ryan Kann <ryanakann@gmail.com>
2019-01-08 02:14:13 -0500

    Obstacles implemented fully

---
+++ Assets/Portal.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+
+    public static Portal lastPortal;
+
+    public Portal pair;
+
+    public float teleportCD = 0f;
+    public float maxTeleportCD = 0.1f;
+
+    // Start is called before the first frame update
+    void Start () {
+        if (lastPortal != null) {
+            pair = lastPortal;
+            lastPortal.pair = this;
+            lastPortal = null;
+        } else {
+            lastPortal = this;
+        }
+    }
+
+    private void Update () {
+        if (teleportCD > 0f) {
+            teleportCD -= Time.deltaTime;
+        } else {
+            teleportCD = 0f;
+        }
+    }
+
+    void TeleportToPair (Transform t) {
+        if (!pair || teleportCD > 0f)
+            return;
+
+        t.position = pair.transform.position;
+        teleportCD = maxTeleportCD;
+        pair.teleportCD = pair.maxTeleportCD;
+    }
+
+    private void OnTriggerEnter2D (Collider2D collision) {
+        if (collision.CompareTag ("Shape") && collision.GetComponent<Rigidbody2D>()) {
+            TeleportToPair (collision.transform);
+        }
+    }
+}
---
+++ Assets/Stop.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stop : MonoBehaviour
+{
+    //private void OnTriggerEnter2D (Collider2D collision) {
+    //    if (collision.CompareTag ("Shape")) {
+    //        if (collision.GetComponent<Rigidbody2D> ()) {
+    //            collision.GetComponent<Rigidbody2D> ().velocity *= 0.75f;
+    //        }
+    //    }
+    //}
+}
