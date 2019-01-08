1db600aa01831e0db031f20d2ee7872683b6a7c0
Ryan Kann <ryanakann@gmail.com>
2019-01-07 21:36:11 -0500

    Shapes!

---
+++ Assets/Shape.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Shape : MonoBehaviour {
+
+    public bool isOnPath = true;
+    public float speed;
+
+    public Vector3[] path;
+    public int currentIndex;
+    public int lastIndex;
+
+    public Vector3 target;
+
+    private Vector3 dampVel;
+
+    Rigidbody2D rb;
+
+    public void SetPath (Vector3[] _path) {
+        print ("Path length: " + _path.Length);
+        path = _path;
+        lastIndex = path.Length - 1;
+    }
+
+    // Start is called before the first frame update
+    void Start () {
+        currentIndex = 1;
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (isOnPath && path.Length > 0) {
+            if (currentIndex < lastIndex) {
+                target = path[currentIndex];
+                transform.up = Vector3.SmoothDamp (transform.up, (target - transform.position).normalized, ref dampVel, 0.3f);
+
+                rb.velocity = (target - path[currentIndex - 1]).normalized * speed;
+
+                if ((transform.position - target).sqrMagnitude < 0.01f) {
+                    currentIndex++;
+                    print ("Next index!");
+                }
+            } else {
+                Destroy (gameObject);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D (Collision2D collision) {
+        if (collision.collider.CompareTag ("Shape")) {
+            isOnPath = false;
+        }
+    }
+}
